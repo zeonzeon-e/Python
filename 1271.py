0e997a261ad51d856afbdf3dc4125a749b0f27e5
zeonzeon-e <julia3277@naver.com>
2023-10-13 15:19:31 +0900

    백준 2475번

--- 1271.py
+++ 1271.py
@@ -1,4 +1,8 @@
-N = input()
+# map : 첫 번째 인자(함수), 두 번째 인자(반복 가능한 자료형)
+# map을 사용하면 list로 다시 한 번 바꿔줘야 한다.
 
-for i in range(1, int(N) + 1):
-    print("*" * i)
+num = map(int, input().split())
+result = 0
+for i in num:
+    result += i**2
+print(result % 10)
