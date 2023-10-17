a7f59f1d3718ce9e8455f8625672365ce4c8e8ef
zeonzeon-e <julia3277@naver.com>
2023-10-17 14:45:05 +0900

    파이썬 백준 2420번 문제

--- 2420.py
+++ 2420.py
@@ -1,9 +1,16 @@
+A, B = [], []
+
 N, M = map(int, input().split())
 
+for row in range(N):
+    row = list(map(int, input().split()))
+    A.append(row)
 
-result = N - M
+for row in range(N):
+    row = list(map(int, input().split()))
+    B.append(row)
 
-if result < 0:
-    result = -(result)
-
-print(result)
+for row in range(N):
+    for col in range(M):
+        print(A[row][col] + B[row][col], end=" ")
+    print()
